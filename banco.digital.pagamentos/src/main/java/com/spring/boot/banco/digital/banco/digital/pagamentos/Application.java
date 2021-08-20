3e425685b269ee44a9677769a9aa4b46ccb68fed
uervitonsantos <uerviton@gmail.com>
2021-08-20 15:37:01 -0300

    commit inicial

---
+++ banco.digital.pagamentos/src/main/java/com/spring/boot/banco/digital/banco/digital/pagamentos/Application.java
@@ -0,0 +1,13 @@
+package com.spring.boot.banco.digital.banco.digital.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ banco.digital.pagamentos/src/test/java/com/spring/boot/banco/digital/banco/digital/pagamentos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.banco.digital.banco.digital.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
